733f34f9a2ac74a1856238f807658a8431236b45
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-11 21:14:26 -0500

    Added links for sections in Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
@@ -9,6 +10,35 @@ const Navbar = () => {
         <Image 
             src='/../public/About-logo.png' alt='/' 
             width='125' height='50'/>
+        <div>
+            <ul className='hidden md:flex'>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>
+                    Home
+                    </li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>
+                    About
+                    </li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>
+                    Skills
+                    </li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>
+                    Projects
+                    </li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>
+                    Contact
+                    </li>
+                </Link>
+            </ul>
+        </div>
       </div>
     </div>
   )
