09692705331d1f09922bb9abc14a1b2d3621697d
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-11 20:45:53 -0500

    Added placeholder logo + Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center
+                      w-full h-full px-2 2xl:px-16'>
+        <Image 
+            src='/../public/About-logo.png' alt='/' 
+            width='125' height='50'/>
+      </div>
+    </div>
   )
 }
 
