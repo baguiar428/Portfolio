d487ec60628cb2c204e99e07f7a71669cb4483b6
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-13 21:19:23 -0400

    Created Placeholders for About Page. Added About component to Index

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+            <div className='col-span-2'>
+                <h2>About Me</h2>
+                {/* Create bio in this div */}
+                <p>Fill In Bio Here</p>
+            </div>
+            {/* Place image below */}
+            <div></div>
+        </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Main from '@/components/Main'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import Main from '@/components/Main';
+import About from '@/components/About';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function Home() {
       </Head>
       <Navbar/>
       <Main/>
+      <About/>
     </>
   )
 }
