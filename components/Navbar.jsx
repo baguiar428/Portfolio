ef44587263b69b09f52a3921ddd46735d905c0ba
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-13 20:16:35 -0400

    Created Main (spotlight) component and started About component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
         </div>
       </div>
       <div
-        className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/70" : ""}
+        className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}
       >
         <div
           className={
