12316e65851849f814efb3a2d794b4e137fc2b76
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-14 17:19:07 -0400

    added more logos. Linked logos into Image Lazy Loading

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,8 +5,8 @@ import css_logo from "public/assets/web_tech_logos/css3.svg";
 import github_logo from "public/assets/web_tech_logos/github.svg";
 import html_logo from "public/assets/web_tech_logos/html5.svg";
 import js_logo from "public/assets/web_tech_logos/javascript.svg";
-import jira from "public/assets/web_tech_logos/jira.svg";
-import node_logo from "public/assets/web_tech_logos/nodejs.svg";
+import jira_logo from "public/assets/web_tech_logos/jira.svg";
+import material_logo from "public/assets/web_tech_logos/material-ui.svg";
 import npm_logo from "public/assets/web_tech_logos/npm.svg";
 import postgres_logo from "public/assets/web_tech_logos/postgresql.svg";
 import react_logo from "public/assets/web_tech_logos/react.svg";
@@ -31,6 +31,138 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>Javascript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={react_logo} width="64" height="64" alt="react logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={npm_logo} width="64" height="64" alt="npm logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>NPM</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={yarn_logo} width="64" height="64" alt="yarn logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Yarn</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={rails_logo} width="64" height="64" alt="ruby on rails logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Ruby on Rails</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={github_logo} width="64" height="64" alt="github logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Github</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={tailwind_logo} width="64" height="64" alt="tailwind logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Tailwind CSS</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={semantic_logo} width="64" height="64" alt="semantic-ui logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Semantic-UI</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={material_logo} width="64" height="64" alt="material-ui logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Material-UI</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={jira_logo} width="64" height="64" alt="jira logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Jira</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={html_logo} width="64" height="64" alt="html logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={css_logo} width="64" height="64" alt="css logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={postgres_logo} width="64" height="64" alt="postgresql logo"/>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>PostgreSQL</h3>
               </div>
             </div>
           </div>
