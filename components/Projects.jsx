95584a5be6d88162d12cd9290e0545a0dbf4db7d
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-15 21:28:42 -0400

    Abstracted out projects with projectItem component, tweaked some styling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import newRecoupImg from "public/assets/projects/new_recoup_collage.png";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
@@ -8,22 +10,17 @@ const Projects = () => {
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p>Projects</p>
         <h2>What I've Built</h2>
-        {/* Container for project */}
+        {/* Container for projects */}
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-green-500 to-green-800">
-            <Image
-              className="rounded-xl group-hover:opacity-10"
-              src={newRecoupImg}
-              alt="New Recoup Project"
-            />
-            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-              <h3>New Recoup Massage</h3>
-              <p>
-                Web portal that allows therapists to keep a contact list and send
-                custom promotional emails and SMS to multiple clients at a time
-              </p>
-            </div>
-          </div>
+          <ProjectItem
+            title="New Recoup"
+            backgroundImg={newRecoupImg}
+            projectUrl="/newrecoup"
+            description="Web portal that allows therapists to keep an address book and
+            send custom promotional emails and SMS to multiple clients at a
+            time."
+            tech="React JS + Ruby on Rails"
+          />
         </div>
       </div>
     </div>
