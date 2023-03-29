f87c7ddb4f90c53071ca65acb22ce984649612a9
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-03-29 17:26:36 -0400

    Added an About Me Bio

--- components/About.jsx
+++ components/About.jsx
@@ -9,20 +9,26 @@ const About = () => {
         <div className="col-span-2">
           <h2>About Me</h2>
           {/* Create bio in this div */}
-          <p>Fill In Bio Here</p>
+          <p>I&apos;m bilingual in English and Portuguese. Besides technology 
+            I enjoy adventure, traveling and food. It&apos;s hard to beat sitting down 
+            with friends for some good conversation and food. I&apos;m a father-to-be which
+            has me excited / terrified but I'm ready to take on the new and exciting challenge.
+            My hobbies include tinkering with technology, snowboarding, MotoGP racing, DIY and gardening.
+          </p>
           {/*Place in link for resume or projects */}
         </div>
         {/* Place image below */}
         <div
-          className="w-full h-auto m-auto shadow-lg shadow-gray-500 rounded-xl
-                            flex items-center justify-center p-2 hover:scale-105 ease-in duration-300"
+          className="w-full h-auto m-auto shadow-lg shadow-[#656565] rounded-xl
+                            flex items-center justify-center"
+                            // Not sure I need/want this: hover:scale-105 ease-in duration-300
         >
           <Image
             className="rounded-xl"
             src={profile_pic}
             alt="Profile Pic"
-            width="200"
-            height="150"
+            width="400"
+            height="300"
           />
         </div>
       </div>
