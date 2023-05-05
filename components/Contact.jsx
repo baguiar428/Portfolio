bbee2b6eff1c6319fa2e06cbd69b1839b7334db7
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-04 21:34:47 -0400

    Removed form and sendgrid. Over engineered for purpose it served. Did not want to pay for custom email and identity for now

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,22 +13,28 @@ import {
 const Contact = () => {
 
   // State for Contact Form
-  const [fullname, setFullname] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  // Removing feature for now. I don't have enough traffic to justify paying for
+  // custom email address. SendGrid is activated and can be used in the future.
+
+  // const [fullname, setFullname] = useState("");
+  // const [phone, setPhone] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [subject, setSubject] = useState("");
+  // const [message, setMessage] = useState("");
 
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         {/* <p className="text-xl tracking-widest uppercase">Contact</p> */}
-        <h2 className="pt-10 pb-4">Get In Touch</h2>
-        <div className="grid lg:grid-cols-5 gap-8">
+        <h2 className="pt-10 pb-4 text-center">Get In Touch</h2>
+        {/* Original Layout for Contact Form
+        <div className="grid lg:grid-cols-5 gap-8"> */}
+        <div className="flex justify-center">
           {/* Left */}
-          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl bg-[#26c485] shadow-[#656565] rounded-xl p-4">
-            <div className="lg:p-4 h-full">
-              <div>
+          {/* <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl bg-[#26c485] shadow-[#656565] rounded-xl p-4"> */}
+          <div className="flex justify-center h-full shadow-xl bg-[#26c485] shadow-[#656565] rounded-xl p-4">
+            <div className="lg:p-4 h-full ">
+              <div className="flex justify-center">
                 <Image
                   className="rounded-xl"
                   //Not sure i want these : hover:scale-105 ease-in duration-300
@@ -37,19 +43,16 @@ const Contact = () => {
                   height="200"
                   alt="Contact Me"
                 />
-                {/* Not sure why this isn't loading... */}
-                {/* <img src={contact_me} alt="Contact Me"/> */}
               </div>
               <div>
-                <h2 className="py-2">Bruno Aguiar</h2>
-                <p>Full Stack Developer</p>
+                <h2 className="py-2 text-center">Bruno Aguiar</h2>
+                <p className="text-center">Full Stack Developer</p>
                 <p className="py-4">
-                  I am available for full-stack web, mobile and application development in freelance or full-time positions. Contact
-                  me and let&apos;s talk.
+                  I am available for full-stack web, mobile and application development in freelance or full-time positions.
                 </p>
               </div>
               <div>
-                <p className="pt-8 uppercase font-bold">Connect With Me</p>
+                <p className="pt-8 uppercase font-bold text-center">Connect With Me</p>
                 {/* Container for icons */}
                 <div className="flex items-center justify-between py-4">
                   <a
@@ -64,20 +67,20 @@ const Contact = () => {
                   <a
                     className="rounded-full shadow-lg bg-[#ff6700] shadow-[#656565]
                                     p-6 cursor-pointer hover:scale-110 ease-in duration-300"
-                    target="_blank"
-                    href="https://github.com/baguiar428"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGithub size={32} className="text-[#050517]"/>
-                  </a>
-                  <a
-                    className="rounded-full shadow-lg bg-[#ff6700] shadow-[#656565]
-                                    p-6 cursor-pointer hover:scale-110 ease-in duration-300"
                   //Copy email to clipboard after clicking icon
                   // onClick={() =>  navigator.clipboard.writeText('b.aguiar428@gmail.com')}
                   href="mailto:b.aguiar428@gmail.com" data-initialized="true"
                   >
                     <FaRegEnvelope size={32} className="text-[#050517]" />
+                  </a>
+                  <a
+                    className="rounded-full shadow-lg bg-[#ff6700] shadow-[#656565]
+                                    p-6 cursor-pointer hover:scale-110 ease-in duration-300"
+                    target="_blank"
+                    href="https://github.com/baguiar428"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub size={32} className="text-[#050517]"/>
                   </a>
                   {/* Use for later */}
                   {/* <div className='rounded-full shadow-lg shadow-gray-500
@@ -89,9 +92,11 @@ const Contact = () => {
             </div>
           </div>
           {/* Right */}
-          <div className="col-span-3 w-full h-auto shadow-xl bg-[#26c485] shadow-[#656565] rounded-xl lg:p-4">
+          {/* <div className="col-span-3 w-full h-auto shadow-xl bg-[#26c485] shadow-[#656565] rounded-xl lg:p-4">
             <div className="p-4">
               <form 
+              // Submit function for SendGrid contact form
+              // Feature not needed for now
               // onSubmit={handleSubmit}
               >
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
@@ -161,7 +166,7 @@ const Contact = () => {
                 <button className="w-full p-4 mt-4">Send Message</button>
               </form>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="flex justify-center py-12">
           <Link href="/">
